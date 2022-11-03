974d1657f7758b7a6c14f9378a80bb8b609e41ed
Danturtle <96111364+Danturtle@users.noreply.github.com>
2022-11-03 10:36:22 +0330

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,64 @@
+a = float(input("Enter your first number : \n"))
+b = float(input("Enter your second number : \n"))
+print("-----------------------------------")
+print("Loading...")
+
+sum1 = a + b
+sub1 = a - b
+mul1 = a * b
+div1 = a / b
+rem1 = a % b
+sum2 = b + a
+sub2 = b - a
+mul2 = b * a
+div2 = b / a
+rem2 = b % a
+
+
+input("Please just press Enter to see your results")
+print(f"Your first number  +  Your second number = {sum1} \n")
+
+print("----------------------------------- \n")
+
+input("Please just press Enter to see your results")
+print(f"Your first number  -  Your second number = {sub1} \n")
+
+print("----------------------------------- \n")
+
+input("Please just press Enter to see your results")
+print(f"Your first number  *  Your second number = {mul1} \n")
+
+print("----------------------------------- \n")
+
+input("Please just press Enter to see your results")
+print(f"Your first number  /  Your second number = {div1} \n")
+
+print("----------------------------------- \n")
+
+input("Please just press Enter to see your results")
+print(f"Your first number  %  Your second number = {rem1} \n")
+
+print("----------------------------------- \n")
+
+input("Please just press Enter to see your results")
+print(f"Your second number  +  Your first number = {sum2} \n")
+
+print("----------------------------------- \n")
+
+input("Please just press Enter to see your results")
+print(f"Your second number  -  Your first number = {sub2} \n")
+
+print("----------------------------------- \n")
+
+input("Please just press Enter to see your results")
+print(f"Your second number  *  Your first number = {mul2} \n")
+
+print("----------------------------------- \n")
+
+input("Please just press Enter to see your results")
+print(f"Your second number  /  Your first number = {div2} \n")
+
+print("----------------------------------- \n")
+
+input("Please just press Enter to see your results")
+print(f"Your second number  %  Your first number = {rem2} \n")
